e20dc3ec00ee49314c560be275e80329e082ba57
PoeticIron <nmestone@gmail.com>
2017-01-05 00:12:04 +1000

    Add files via upload

--- Testbot.py
+++ Testbot.py
@@ -17,8 +17,12 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 @bot.command()
-async def search(text : str):
+async def search(*text : str):
+    finaltext = " "
 
+    for word in text:
+        finaltext = finaltext + word + " "
+    
     api_key = "AIzaSyDEI9ei37MeTgaDyQhayyXSdHPM8ZJ4Gfk"
     cse_id = "001464282721790659668:_ja4f_we2rk"
 
@@ -28,7 +32,7 @@ async def search(text : str):
         pprint.pprint(res)
         return res['items']
 
-    results = google_search(text, api_key, cse_id, num=1, searchType= 'image')
+    results = google_search(finaltext, api_key, cse_id, num=1, searchType= 'image')
     for result in results:
         await bot.say(result['link'])
 
@@ -50,12 +54,22 @@ async def roll(dice : str):
 async def choose(*choices : str):
     """Chooses between multiple choices."""
     await bot.say(random.choice(choices))
-
+@bot.command()
+async def KrillMe():
+    await bot.say("http://coolaustralia.org/wp-content/uploads/2013/05/billandwill.jpg")
+@bot.command()
+async def Mods():
+    await bot.say("Don't mind me, just taking my hotdogs for a walk ( ͡° ͜ʖ ͡°)╯╲___ :hotdog:")
+@bot.command()
+async def Stalin():
+    await bot.say("http://imgur.com/gallery/jXMUIYP")
 @bot.command(description='Actually implemented by me')
 async def Farage():
     """Does largely Nothing"""
     await bot.say("You can't barrage the farage! http://www.thereveillenwu.com/wp-content/uploads/2016/06/6a00d8341bf8f353ef017eeacb1b2e970d.jpg");
-  
+@bot.command()
+async def Kojimbo():
+    await bot.say("http://static1.gamespot.com/uploads/original/43/434805/3064712-1248675325-Hideo.jpg")
 @bot.command()
 async def repeat(times : int, content='repeating...'):
     """Repeats a message multiple times."""
